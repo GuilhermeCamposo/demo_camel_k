fa70b2fe63076112049e19788ec96a93c7a50d40
Guilherme Camposo <guilherme.camposo@me.com>
2022-12-20 07:45:31 -0500

    Initial Commit

---
+++ camel_k/Client.java
@@ -0,0 +1,18 @@
+// camel-k: language=java
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.Exchange;
+
+public class Client extends RouteBuilder {
+  @Override
+  public void configure() throws Exception {
+
+      from("timer:java?repeatCount=10&period=2s")
+        .routeId("java")
+        .setBody(simple("{ \"value\": \"${date:now:yyyy-MM-dd-HHmmssSSS}\"}"))
+        .setHeader(Exchange.CONTENT_TYPE,constant("application/json"))
+        .to("http://api/v1/camelk?httpMethod=PUT")
+        .log("response: ${body}");
+
+  }
+}
